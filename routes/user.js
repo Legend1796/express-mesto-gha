245bd32c1e2545481545c4b376a8360726679d65
Igor Manukhov <m_igor97@mail.ru>
2022-08-26 16:48:11 +0300

    update post userS

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const {
 
 userRouters.get('/users', express.json(), getUsers);
 userRouters.get('/users/:userId', express.json(), getUser);
-userRouters.post('/user', express.json(), createUser);
+userRouters.post('/users', express.json(), createUser);
 userRouters.post('/user/me', express.json(), updateUser);
 userRouters.post('/user/me/avatar', express.json(), updateUserAvatar);
 
